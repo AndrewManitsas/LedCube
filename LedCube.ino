ffd3a1d4dd77c3802d69a76a8f37979374e4acfe
Ανδρέας Μανίτσας <andrewmanitsas@gmail.com>
2017-08-31 18:49:37 +0300

    added arduino files

---
+++ LedCube.ino
@@ -0,0 +1,233 @@
+#include <Wire.h>
+
+/*
+ * UNIVERSITY of WESTERN MACEDONIA
+ *        ICTE Department
+ * 
+ * ARDUINO LED CUBE
+ * 
+ * -----------Made by-----------------
+ * George Kortsaridis
+ * email : gkortsaridis@gmail.com
+ * web   : gkortsaridis.noip.me
+ * 
+ * --------Supervised by-------------
+ * Dasygenis Minas
+ * email : mdasyg@ieee.com
+ * web   : arch.icte.uowm.gr
+ * -----------------------------------
+ * 
+ * ----Expanded with web interface----
+ * Andrew Manitsas
+ * email : andrewmanitsas@gmail.com
+ * LinkedIn : www.linkedin.com/in/andrewmanitsas
+ */
+
+int column[] =
+{
+  30,28,26,24,22,
+  31,29,27,25,23,
+  41,39,37,35,33,
+  44,42,47,45,43,
+  40,38,36,34,32
+};
+
+int row[] = {7,5,3,6,4};
+
+int irLed = A0;
+int irInput = A1;
+int irBackup;
+
+int state = 2; //This variable controls the default patterns. Acts as selector
+
+int testAnimation[] = {0,1,2,3,4,5};
+
+const int anim_frames = 1;
+int animation[anim_frames][125] = {{0,0,1,0,0,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  //Serial3.begin(9600);  
+
+  Wire.begin(); //Initialize the I2C communication between the arduinos
+  
+  for(int i=0; i<27; i++)
+  {
+    pinMode(column[i],OUTPUT);
+  }
+
+  for(int i=0; i<5; i++)
+  {
+    pinMode(row[i],OUTPUT);
+  }
+
+  pinMode(irLed, OUTPUT);
+  digitalWrite(irLed,HIGH);
+  delay(1000);
+  irBackup = analogRead(irInput);
+}
+
+void receiveEvent(int bytes)
+{
+  state = Wire.read(); // read one character from the I2C
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+  Wire.begin(9);
+  Wire.onReceive(receiveEvent);
+
+  if(state == '0')
+  {
+    allOff();
+  }
+  else if(state == '1')
+  {
+    allOn();
+  }
+  else if(state == '2')
+  {
+    ledRun(50);
+  }
+  else if(state == '3')
+  {
+    columnRun(100);
+  }
+  else if(state == '4')
+  {
+    randomParty(25);
+  }
+  else if(state == '5')
+  {
+    reflective();
+  }
+  else if(state == '6')
+  {
+    fromIntArrayToState(testAnimation, 250, 6);
+  }
+  else if(state == '7'){
+    //Here get the custom pattern
+    
+    fromIntIntArrayToAnimation(250);
+  }
+}
+
+void led(int which,int what)
+{
+  digitalWrite(row[which/25],what);
+  digitalWrite(column[which%25],what);
+}
+
+void columnRun(int del)
+{
+  allOff();
+  
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(row[i],HIGH);
+  }
+
+  for(int i=0; i<25; i++)
+  {
+    digitalWrite(column[i],HIGH);
+    delay(del);
+  }
+}
+
+void ledRun(int del)
+{
+  for(int i=0; i<125; i++)
+  {
+    led(i,HIGH);
+    delay(del);
+    led(i,LOW);
+  }
+}
+
+void allOn()
+{
+  for(int i=0; i<25; i++)
+  {
+    digitalWrite(column[i],HIGH);
+  }
+  
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(row[i],HIGH);
+  }
+}
+
+void allOff()
+{
+  for(int i=0; i<25; i++)
+  {
+    digitalWrite(column[i],LOW);
+  }
+  
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(row[i],LOW);
+  }
+}
+
+void randomParty(int del)
+{
+  for(int i=0; i<100; i++)
+  {
+    int l = random(0,124);
+    led(l,HIGH);
+    delay(del);
+    led(l,LOW);  
+  }
+}
+
+void reflective()
+{
+  int val = analogRead(irInput);    // read the input pin
+  
+  if(val < irBackup)
+  {
+    allOn();
+  }
+  else
+  {
+    allOff();
+  }
+  
+  Serial.println(val); 
+}
+
+void fromIntArrayToState(int intarray[] , int del, int arraySize)
+{
+    for(int i=0; i<arraySize; i++)
+    {
+      if(intarray[i] >= 0 && intarray[i] <= 124)
+      {
+        led(intarray[i],HIGH);
+        delay(del);
+      }
+    }
+    
+    allOff();
+}
+
+void fromIntIntArrayToAnimation(int del)
+{
+  for(int i=0; i<anim_frames; i++)
+  {
+    allOff();
+    
+    for(int j=0; j<125; j++)
+    {
+       led(j, animation[i][j]);
+       delay(1);
+    }
+    
+    delay(del);
+  }
+}
---
+++ WiFi_LED_Controller.ino
@@ -0,0 +1,202 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "ZTE ZXHN-H208N";      //  your network SSID (name)
+//char ssid[] = "UOWM-K7-WIFI";
+//char ssid[] = "ZTE ZXHN-H108NS";
+
+char pass[] = "!admin7720!";   // your network password
+//char pass[] = "uowm7P123897";
+//char pass[] = "tanar1953";
+
+int status = WL_IDLE_STATUS;
+
+// Initialize the Wifi client library
+WiFiClient client;
+
+// server address:
+char server[] = "zafora.icte.uowm.gr";
+
+unsigned long lastConnectionTime = 0; // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
+
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+
+  Wire.begin();
+
+  // check for the presence of the WiFi shield:
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+//This method prints the status of the wireless connection
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+// this method makes a HTTP connection to the server:
+void httpRequest()
+{
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
+  client.stop();
+
+  // if there's a successful connection:
+  if (client.connect(server, 80))
+  {
+    //Serial.println("connecting...");
+    client.println("GET /~ictest00709/led_cube/com_files/default_patterns.txt");
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  }
+  else
+  {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
+  }
+}
+
+void loop()
+{
+  // if there's incoming data from the net connection.
+  // send it out the serial port.  This is for debugging
+  // purposes only:
+  String pattern;
+  
+  while (client.available())
+  {
+    char c = client.read(); //HERE IS THE NUMBER FROM THE SERVER
+
+    //The number we get is the pattern selector
+    pattern = c;
+    ledPatternSelect(pattern);
+  }
+  
+  // if ten seconds have passed since your last connection,
+  // then connect again and send data:
+  if (millis() - lastConnectionTime > postingInterval)
+  {
+    httpRequest();
+    ledPatternSelect(pattern);
+
+    if(pattern.equals("7"))
+    {
+      Serial.println("Custom pattern");
+    }
+  }
+}
+
+void ledPatternSelect(String pattern)
+{  
+  if(pattern.equals("0")) //State 0
+  {
+    Serial.println("All OFF");
+
+    Wire.beginTransmission(9);
+    Wire.write('0');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("1")) //State 1
+  {
+    Serial.println("All ON");
+
+    Wire.beginTransmission(9);
+    Wire.write('1');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("2")) //State 2
+  {
+    Serial.println("Led Run");
+
+    Wire.beginTransmission(9);
+    Wire.write('2');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("3")) //State 3
+  {
+    Serial.println("Column Run");
+
+    Wire.beginTransmission(9);
+    Wire.write('3');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("4")) //State 4
+  {
+    Serial.println("Random Party");
+
+    Wire.beginTransmission(9);
+    Wire.write('4');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("5")) //State 5
+  {
+    Serial.println("Reflective");
+
+    Wire.beginTransmission(9);
+    Wire.write('5');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("6")) //State 6
+  {
+    Serial.println("Test Animation");
+
+    Wire.beginTransmission(9);
+    Wire.write('6');
+    Wire.endTransmission();
+  }
+
+  if(pattern.equals("7")) //State 7
+  {
+    Serial.println("Custom Pattern");
+
+    Wire.beginTransmission(9);
+    Wire.write('7');
+    Wire.endTransmission();
+  }
+}
